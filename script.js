76a67ad938f7d3733ca0b72aaf48c4e8a6b290d6
Melby01 <melby.okozi@student.moringaschool.com>
2021-04-18 01:32:43 +0300

    Update the what we do page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
- 
+$(document).ready(function () {
+    $("#one").click(function () {
+      $(".design").toggle();
+    });
+  
+    $("#two").click(function () {
+      $(".dev").toggle();
+    });
+    $("#three").click(function () {
+      $(".dev").toggle();
+    });
+  });
+  

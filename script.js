aaec86526c988eb301e9bb5a53c808a3096d3304
Melby01 <melby.okozi@student.moringaschool.com>
2021-04-18 20:32:38 +0300

    Update the portfolio page

--- script.js
+++ script.js
@@ -10,4 +10,79 @@ $(document).ready(function () {
       $(".pro").toggle();
     });
   });
+
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#four").show();
+  
+    })
+    .mouseout(function(){
+      $("#four").hide();
+    })
+    
+    $("#work3").mouseover(function(){
+      $("#security").show();
+  
+    })
+    .mouseout(function(){
+      $("#security").hide();
+    })
+    
+    $("#work2").mouseover(function(){
+      $("#empower").show();
+  
+    })
+    .mouseout(function(){
+      $("#empower").hide();
+    })
+  
+  
+    
+    $("#work1").mouseover(function(){
+      $("#curry").show();
+  
+    })
+    .mouseout(function(){
+      $("#curry").hide();
+    })
+  
+    $("#work5").mouseover(function(){
+      $("#website").show();
+  
+    })
+    .mouseout(function(){
+      $("#website").hide();
+    })
+  
+  
+    $("#work6").mouseover(function(){
+      $("#calculator").show();
+  
+    })
+    .mouseout(function(){
+      $("#calculator").hide();
+    })
+  
+  
+    $("#work7").mouseover(function(){
+      $("#bun").show();
+  
+    })
+    .mouseout(function(){
+      $("#bun").hide();
+    })
+  
+  
+    $("#work8").mouseover(function(){
+      $("#rest").show();
+  
+    })
+    .mouseout(function(){
+      $("#rest").hide();
+    })
+  
+  
+  
+  
+  });
   
